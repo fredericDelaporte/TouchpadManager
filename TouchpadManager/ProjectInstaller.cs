b3803870b1899a6fe51b5ff76983da1931cdc6e6
Frédéric Delaporte <fredericdelaporte@free.fr>
2017-11-11 18:29:17 +0100

    Add service installer.

---
+++ TouchpadManager/ProjectInstaller.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace TouchpadManager
+{
+    /// <summary>
+    /// Service installer
+    /// </summary>
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+    }
+}
